890c7b79ca10ed7b186bcef3a3ce4cdc98de4409
0rkida <orkida027@gmail.com>
2025-08-06 22:57:44 +0200

    Project Folder Structure

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+
+const router = createBrowserRouter([
+
+
+
+
+function App() {
+  return (
+    <h1>Hello World</h1>
+  );
+}
+
+export default App;
---
+++ client/src/pages/ElectionsDetails.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ElectionsDetails = () => {
+  return (
+    <div>ElectionsDetails</div>
+  )
+}
+
+export default ElectionsDetails
