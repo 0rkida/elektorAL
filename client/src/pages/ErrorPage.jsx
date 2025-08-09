ca91172078f41b193932a820fdd457610749f8f2
0rkida <orkida027@gmail.com>
2025-08-10 00:25:31 +0200

    Fixed error page

--- client/src/pages/ErrorPage.jsx
+++ client/src/pages/ErrorPage.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
-
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom';
+import error from '../assets/error.gif';
 const ErrorPage = () => {
+  const navigate = useNavigate();
+
+  //redirect to previous page after 5 seconds
+  useEffect(() => {
+    setTimeout(() => {
+      navigate(-1);
+    }, 5000); 
+  })
+
   return (
-    <div>ErrorPage</div>
+    <section className ="errorPage">
+    <div className="errorContent">
+      <img src={error} alt = "Page not found" />
+      <h1>404</h1>
+      <p>This page does not exist. You will be directed to the previous page shortly</p>
+      </div>
+      </section>
+    
   )
 }
 
