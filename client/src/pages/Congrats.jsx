7dc3a3c55d1b01eeb733b7e8f5d94b22fcd4065d
0rkida <orkida027@gmail.com>
2025-08-21 12:39:31 +0200

    Added the congrats page

--- client/src/pages/Congrats.jsx
+++ client/src/pages/Congrats.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import { FaCheckCircle } from "react-icons/fa";
 
 const Congrats = () => {
   return (
-    <div>Congrats</div>
+    <section className="congrats">
+      <div className="container congrats__container">
+        <FaCheckCircle className="congrats__icon" />
+        <h2>Faleminderit për shprehjen e vullnetit tuaj të lirë!</h2>
+        <p>Vota juaj u shtua tek kandidati që zgjodhët. Pas pak do të drejtoheni te faqja e rezultateve.</p>
+
+        <Link to="/results" className="btn sm primary">Shiko rezultatet</Link>
+      </div>
+    </section>
   )
 }
 
