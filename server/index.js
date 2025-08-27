e66f70a03464ef29d4b50d91be73d0bfbe7481c0
0rkida <orkida027@gmail.com>
2025-08-27 13:15:01 +0200

    Connected with the database

--- server/index.js
+++ server/index.js
@@ -4,10 +4,16 @@ const {connect} = require("mongoose")
 
 require("dotenv").config()
 
+const Routes = require("./routes/Routes")
+
 const app = express()
 app.use(express.json({extended: true}))
 app.use(express.urlencoded({extended: true}))
 app.use(cors({credentials: true, origin: ["http://localhost300"]}))
 
-app.listen(process.env.PORT , () => console.log(`Server started on port ${process.env.PORT}`))
+app.use('/api', Routes)
+
+
+connect(process.env.MONGO_URL).then(app.listen(process.env.PORT , () => console.log
+(`Server started on port ${process.env.PORT}`))).catch(err => console.log(err))
 
