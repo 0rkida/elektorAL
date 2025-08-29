711dc0694ffc14557be373ef5559fbbf4b8df856
0rkida <orkida027@gmail.com>
2025-08-29 11:51:15 +0200

    Added the models, fixed one part of the voter controller

--- server/controllers/voterController.js
+++ server/controllers/voterController.js
@@ -1,13 +1,61 @@
+const bcrypt = require('bcryptjs')
+
+VoterModel = require('../models/voterModel')
+const HttpError = require('../models/ErrorModel')
 
 // ============= REGISTER NEW VOTER
 //POST : api/voters/register
-const registerVoter = (req, res, next) => {
-    res.json("Register Voter")
+const registerVoter = async (req, res, next) => {
+    try {
+        const {fullName, email, password, password2} = req.body;
+        if ( !fullName || !email || !password || !password2) {
+            return next(new HttpError("Mbushni te gjitha fushat ", 442))
+        }
+
+        //make all emails lowercased
+        const newEmail = email.toLowerCase()
+
+        //check if email exists in db
+        const emailExists = await VoterModel.findOne({email: newEmail})
+        if(emailExists) {
+            return next (new HttpError("Ky email eshte i regjistruar !", 442))
+        }
+
+        //make sure password 6+ chars
+        if ((password.trim().length) < 6) {
+            return next(new HttpError("Fjalekalimi duhet te jete te pakten 6 karaktere", 422))
+        }
+
+        //make sure passwords march
+        if (password != password2){
+            return next(new HttpError("Fjalekalimet nuk perputhen", 422))
+        }
+
+        //hash password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        //no user/voter should be admin
+
+        let isAdmin = false;
+        if (newEmail == "achiever@gmail.com"){
+            isAdmin == true
+        }
+
+        //save new voter to db
+        const newVoter = await VoterModel.create({fullName, email: newEmail, password: hashedPassword, isAdmin})
+        res.status(201).json(`Votues i ri ${fullName} u krijua.`)
+
+        
+    } catch (error) {
+        return next(new HttpError("Regjistrimi i votuesit deshtoi. ", 422))
+    }
+    
 }
 
-// ============= OGIN VOTER
+// ============= LOGIN VOTER
 //POST : api/voters/login
-const loginVoter = (req, res, next) => {
+const loginVoter = async (req, res, next) => {
     res.json("Login Voter")
 }
 
@@ -15,7 +63,7 @@ const loginVoter = (req, res, next) => {
 //GET : api/voters/:ID
 //PROTECTED
 
-const getVoter = (req, res, next) => {
+const getVoter = async (req, res, next) => {
     res.json("Get Voter")
 }
 
