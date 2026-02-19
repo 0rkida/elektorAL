507adac1ebcb918f5d94737e913a57d75fad8636
0rkida <orkida027@gmail.com>
2026-02-19 13:10:35 +0100

    Fixed the election details to fetch them from the database

--- client/src/pages/ElectionDetails.jsx
+++ client/src/pages/ElectionDetails.jsx
@@ -1,50 +1,102 @@
-import React from 'react'
-import { elections, voters } from '../data'
+import { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
-import { candidates } from '../data'
 import ElectionCandidate from '../components/ElectionCandidate'
 import { IoAddOutline } from "react-icons/io5";
 import { useDispatch, useSelector } from 'react-redux'
 import { uiActions } from '../store/ui-slice'
 import AddCandidateModal from '../components/AddCandidateModal'
+import axios from 'axios'
+import { useState } from 'react'
 
 
 const ElectionsDetails = () => {
+  const [isLoading, setIsLoading] = useState(false)
+  const [election, setElection] = useState({})
+  const [candidates, setCandidates] = useState([])
+  const [voters, setVoters] = useState([])
 
   const {id} = useParams()
   const dispatch = useDispatch()
-
-  const currentElection = elections.find(election => election.id === id) 
-
-  const electionCandidates = candidates.filter(candidate => candidate.election === id)
+  const token = useSelector(state => state.vote.currentVoter?.token)
 
   const addCandidateModalShowing = useSelector(state => state.ui.addCandidateModalShowing)
+  
+  const getElection =async () => {
+    setIsLoading(true)
+    try {
+      const response = await axios.get(`${process.env.REACT_APP_API_URL}/elections/${id}`,
+        {withCredentials: true , headers: {Authorization: `Bearer ${token}`}}
+      )
+      setElection(response.data)
+    } catch (error) {
+      console.log(error)
+    } 
+  }
+
+  const getCandidates = async () => {
+    try {
+      const response = await axios.get(`${process.env.REACT_APP_API_URL}/elections/${id}/candidates`,
+        {withCredentials: true , headers: {Authorization: `Bearer ${token}`}}
+      )
+      setCandidates(response.data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const getVoters = async () => {
+    try {
+      const response = await axios.get(`${process.env.REACT_APP_API_URL}/elections/${id}/voters`,
+        {withCredentials: true , headers: {Authorization: `Bearer ${token}`}}
+      )
+      setVoters(response.data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+
+  useEffect(() => {
+    getElection()
+    getCandidates()
+    getVoters()
+  }, [])   
 
   const openModal = () => {
     dispatch(uiActions.openAddCandidateModal())
   }
 
-
-  return (
-    <>
+return (
+  <>
     <section className="electionDetails">
       <div className="container electionDetails__container">
-        <h2>{currentElection.title}</h2>
-        <p>{currentElection.description}</p>
 
-        <div className="electionDetails__image">
-          <img src={currentElection.thumbnail} alt={currentElection.title} />
-        </div>
+        <h2>{election.title}</h2>
+        <p>{election.description}</p>
+
+       <div className="electionDetails__image">
+  {election.thumbnail && (
+    <img
+      src={election.thumbnail}  
+      alt={election.title || ""}
+    />
+  )}
+</div>
 
         <menu className="electionDetails__candidates">
-          {electionCandidates.map(candidate => <ElectionCandidate key={candidate.id}{...candidate} />)
-          }
+          {candidates.map(c => (
+  <ElectionCandidate
+    key={c._id}
+    name={c.fullName}
+    image={c.image}   
+    motto={c.motto}
+  />
+))}
+
 
           <button className="add__candidate-btn" onClick={openModal}>
-          <IoAddOutline />
-
+            <IoAddOutline />
           </button>
-        
         </menu>
 
         <menu className="voters">
@@ -58,21 +110,23 @@ const ElectionsDetails = () => {
               </tr>
             </thead>
             <tbody>
-              {voters.map(voter => <tr key = {voter.id} >
-                <td><h5>{voter.fullName}</h5></td>
-                <td><h5>{voter.email}</h5></td>
-                <td>14:43:34</td>
-              </tr>)}
+              {voters.map(voter => (
+                <tr key={voter._id}>
+                  <td><h5>{voter.fullName}</h5></td>
+                  <td><h5>{voter.email}</h5></td>
+                  <td>{voter.createdAt}</td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </menu>
+
       </div>
-
-
     </section>
-{ addCandidateModalShowing &&  <AddCandidateModal />
-}    </>
-  )
+
+    {addCandidateModalShowing && <AddCandidateModal />}
+  </>
+)
 }
 
 export default ElectionsDetails
